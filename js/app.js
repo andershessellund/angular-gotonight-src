52410ee33d96e8b6df0043f60863f6934cdc6c05
Anders Hessellund Jensen <ahj@trifork.com>
2014-09-15 13:18:04 +0200

    Step1: Vis liste af TODOs

--- js/app.js
+++ js/app.js
@@ -4,4 +4,21 @@
   "use strict";
 
   var module = angular.module('todomvc', []);
+  module.controller('TodoCtrl', function($scope) {
+    $scope.todos = [
+      { title: 'Vis liste af eksisterende TODOs (en mock-liste til at starte med).' },
+      { title: 'Markér TODO som færdig ved at klikke på flueben.' },
+      { title: 'Skriv ny TODO ved at angive tekst og trykke enter.' },
+      { title: 'Fjern eksisterende TODO ved at trykke på krydset.' },
+      { title: '"XXX items left" skal vise korrekt antal.' },
+      { title: '"Clear completed (XXX)" skal rydde færdige TODOs og vise korrekt antal.' },
+      { title: 'Vis kun "main"-sektion hvis der overhovedet er nogen TODOs.' },
+      { title: 'Vis kun "footer"-sektion hvis der overhovedet er nogen TODOs.' },
+      { title: 'Mulighed for at markere/fravælge alle.' },
+      { title: 'Gem listen af TODOs i localStorage.' },
+      { title: '"All"/"Active"/"Completed" skal vise relevante TODOs.' },
+      { title: 'Den aktuelle side ("All"/"Active"/"Completed") skal fremhæves.' },
+      { title: 'Redigér eksisterende TODO ved at dobbeltklikke på teksten og redigere.' }
+    ];
+  });
 })();
